9fb15b5c50d23fb496dcd1ed0f7d49de9ee5478c
Mateo <rolandoliendo2@gmail.com>
2026-06-13 16:56:58 -0500

    first commit

---
+++ src/main/java/com/examenT2/model/OrdenCompra.java
@@ -0,0 +1,40 @@
+package com.examenT2.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbl_orden_compra")
+@Getter @Setter
+public class OrdenCompra {
+		@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name = "nro_orden")
+	    private Integer nroOrden;
+
+	    @Column(name = "monto")
+	    private Double monto;
+
+	    @Column(name = "direccion_entrega")
+	    private String direccionEntrega;
+
+	    @Column(name = "fecha_entrega")
+	    private LocalDate fechaEntrega;
+
+	    @Column(name = "estado")
+	    private String estado;
+
+	    @ManyToOne
+	    @JoinColumn(name = "id_proveedor")
+	    private Proveedor proveedor;
+}
---
+++ src/main/java/com/examenT2/model/Proveedor.java
@@ -0,0 +1,45 @@
+package com.examenT2.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "tbl_proveedor")
+@Getter
+@Setter
+public class Proveedor {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proveedor")
+    private Integer idProveedor;
+
+    @Column(name = "ruc")
+    private String ruc;
+
+    @Column(name = "razon_social")
+    private String razonSocial;
+
+    @Column(name = "fecha_registro")
+    private LocalDate fechaRegistro;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "pagina_web")
+    private String paginaWeb;
+
+    @ManyToOne
+    @JoinColumn(name = "id_rubro")
+    private Rubro rubro;
+}
